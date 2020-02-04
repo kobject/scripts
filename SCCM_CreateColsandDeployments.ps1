41dcc67f9ea4ff9f8824145c94941d58f599eb52
Joey <joeykerkhof@gmail.com>
2020-02-04 16:29:22 +0100

    remote management added

--- SCCM_CreateColsandDeployments.ps1
+++ SCCM_CreateColsandDeployments.ps1
@@ -1,47 +1,56 @@
 ﻿$smssitecode = "SMSSITECODE"
-$LimitingCollections = "All Users"
+$limitingcollection = "All Users"
 $groups = import-csv FILEPATH 
 $dpgroupname = "DPGROUPNAME"
 
+#Sitecode
+$smssitecode = "OT1"
+
 #Import SCCM Module
 Import-Module $env:SMS_ADMIN_UI_PATH.Replace("\bin\i386", "\bin\configurationmanager.psd1")
+
+#Connect to SCCM Remote uncomment this
+#$SCCMHost = "SCCMSERVERFQDN"
+#if((Get-PSDrive -Name $smssitecode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+#   New-PSDrive -Name $smssitecode -PSProvider CMSite -Root $SCCMHost}
+
 Set-Location "$($smssitecode):"
 
 #Create collection
-foreach ($Group in $groups){
-      if (Get-CMUserCollection -name $($Group.name)){
-      Write-Host -Foregroundcolor Yellow "Usercollection $($Group.name) already exists..."        
+foreach ($group in $groups){
+      if (Get-CMUserCollection -name $($group.name)){
+      Write-Host -Foregroundcolor Yellow "Usercollection $($group.name) already exists..."        
       }
          Else
        {
-            Write-Host -Foregroundcolor Yellow "Create new User Collection $($Group.name)"
-            $QueryGroup = "DOMAIN\\"+"$($Group.name)"
-            $Query = "select SMS_R_USER.ResourceID,SMS_R_USER.ResourceType,SMS_R_USER.Name,SMS_R_USER.UniqueUserName,SMS_R_USER.WindowsNTDomain from SMS_R_User where SMS_R_User.SecurityGroupName = `"$QueryGroup`""
-            New-CMUserCollection -Name $($Group.name) -LimitingCollectionName $LimitingCollections
+            Write-Host -Foregroundcolor Yellow "Create new User Collection $($group.name)"
+            $querygroup = "DOMAIN\\"+"$($group.name)"
+            $Query = "select SMS_R_USER.ResourceID,SMS_R_USER.ResourceType,SMS_R_USER.Name,SMS_R_USER.UniqueUserName,SMS_R_USER.WindowsNTDomain from SMS_R_User where SMS_R_User.SecurityGroupName = `"$querygroup`""
+            New-CMUserCollection -Name $($group.name) -LimitingCollectionName $limitingcollection
             Sleep 1
-            Add-CMUserCollectionQueryMembershipRule -CollectionName $($Group.name) -QueryExpression $Query -RuleName $($Group.name)
+            Add-CMUserCollectionQueryMembershipRule -CollectionName $($group.name) -QueryExpression $Query -RuleName $($group.name)
             }
  
 }
 
 #Move collection to folder
-foreach ($Group in $groups){
-   Write-Host -ForegroundColor Yellow "Move collection $($Group.name) to MSI folder..."
-   $destfolder = "<SMSSITECODE>:\UserCollection\Application\MSI"
-   $collid =  Get-CMCollection -Name $($Group.name)
+foreach ($group in $groups){
+   Write-Host -ForegroundColor Yellow "Move collection $($group.name) to MSI folder..."
+   $destfolder = "$($smssitecode):\UserCollection\Application\MSI"
+   $collid =  Get-CMCollection -Name $($group.name)
    Move-CMObject -InputObject $collid -FolderPath $destfolder    
 }
 
 #Create deployment
-foreach ($Group in $groups){
-    if(Get-CMApplicationDeployment -name $Group.name.Substring(2)){
-    Write-Host -Foregroundcolor Yellow "Deployment exists for $($Group.name.Substring(2))"
+foreach ($group in $groups){
+    if(Get-CMApplicationDeployment -name $group.name.Substring(2)){
+    Write-Host -Foregroundcolor Yellow "Deployment exists for $($group.name.Substring(2))"
     }
     Else{
     Write-Host -Foregroundcolor Yellow "Distribute content to All Distribution Points.."
-    Start-CMContentDistribution -Applicationname $Group.name.Substring(2) -DistributionPointGroupName $dpgroupname
-    Write-Host -Foregroundcolor Yellow "Create Application Deployment for $($Group.name)"
-    New-CMApplicationDeployment -Collectionname $Group.name -Name $Group.name.Substring(2) -DeployAction Install -DeployPurpose Available -Usernotification DisplayAll -AvailableDateTime (get-date) -TimeBaseOn LocalTime
+    Start-CMContentDistribution -Applicationname $group.name.Substring(2) -DistributionPointGroupName $dpgroupname
+    Write-Host -Foregroundcolor Yellow "Create Application Deployment for $($group.name)"
+    New-CMApplicationDeployment -Collectionname $group.name -Name $group.name.Substring(2) -DeployAction Install -DeployPurpose Available -Usernotification DisplayAll -AvailableDateTime (get-date) -TimeBaseOn LocalTime
     }
 }
 

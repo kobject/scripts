07d58addeeb8bb2e0883ae84d11148cce773b3dd
Joey <joeykerkhof@gmail.com>
2020-05-27 09:26:58 +0200

    Error output + add ad group if not exists

--- SCCM_CreateColsandDeployments.ps1
+++ SCCM_CreateColsandDeployments.ps1
@@ -1,20 +1,31 @@
-﻿$smssitecode = "SMSSITECODE"
-$limitingcollection = "All Users"
-$groups = import-csv FILEPATH 
-$dpgroupname = "DPGROUPNAME"
+﻿$limitingcollection = "All Users"
+$groups = import-csv c:\Temp\sccm_create_col.csv
+$dpgroupname = "All DP's on-premises"
+$smssitecode = "XX"
 
-#Sitecode
-$smssitecode = "OT1"
+
+foreach ($adgroup in $groups){
+    $groupname = $($adgroup.name)    
+    $obj =  Get-ADGroup -LDAPFilter "(SAMAccountName=$groupname)"
+    if($obj -eq $null){
+    Write-Host -Foregroundcolor Yellow "Creating security group in Active Directory..."   
+    New-ADGroup –name “$($adgroup.name)” –groupscope Global –path "AD CN PATH"
+    }else{
+    Write-Host -Foregroundcolor Yellow "Group already exists"  
+    }
+}
+
 
 #Import SCCM Module
 Import-Module $env:SMS_ADMIN_UI_PATH.Replace("\bin\i386", "\bin\configurationmanager.psd1")
 
-#Connect to SCCM Remote uncomment this
-#$SCCMHost = "SCCMSERVERFQDN"
+#Connect to SCCM Remote uncomment this\
+#$SCCMHost = "SSCCM FQDN"
 #if((Get-PSDrive -Name $smssitecode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
 #   New-PSDrive -Name $smssitecode -PSProvider CMSite -Root $SCCMHost}
 
 Set-Location "$($smssitecode):"
+
 
 #Create collection
 foreach ($group in $groups){
@@ -26,9 +37,9 @@ foreach ($group in $groups){
             Write-Host -Foregroundcolor Yellow "Create new User Collection $($group.name)"
             $querygroup = "DOMAIN\\"+"$($group.name)"
             $Query = "select SMS_R_USER.ResourceID,SMS_R_USER.ResourceType,SMS_R_USER.Name,SMS_R_USER.UniqueUserName,SMS_R_USER.WindowsNTDomain from SMS_R_User where SMS_R_User.SecurityGroupName = `"$querygroup`""
-            New-CMUserCollection -Name $($group.name) -LimitingCollectionName $limitingcollection
+            New-CMUserCollection -Name $($group.name) -LimitingCollectionName $limitingcollection -RefreshType Both | Out-Null
             Sleep 1
-            Add-CMUserCollectionQueryMembershipRule -CollectionName $($group.name) -QueryExpression $Query -RuleName $($group.name)
+            Add-CMUserCollectionQueryMembershipRule -CollectionName $($group.name) -QueryExpression $Query -RuleName $($group.name) | Out-Null
             }
  
 }
@@ -36,9 +47,9 @@ foreach ($group in $groups){
 #Move collection to folder
 foreach ($group in $groups){
    Write-Host -ForegroundColor Yellow "Move collection $($group.name) to MSI folder..."
-   $destfolder = "$($smssitecode):\UserCollection\Application\MSI"
+   $destfolder = "$($smssitecode):\UserCollection\Applications\MSI"
    $collid =  Get-CMCollection -Name $($group.name)
-   Move-CMObject -InputObject $collid -FolderPath $destfolder    
+   Move-CMObject -InputObject $collid -FolderPath $destfolder | Out-Null   
 }
 
 #Create deployment
@@ -48,9 +59,8 @@ foreach ($group in $groups){
     }
     Else{
     Write-Host -Foregroundcolor Yellow "Distribute content to All Distribution Points.."
-    Start-CMContentDistribution -Applicationname $group.name.Substring(2) -DistributionPointGroupName $dpgroupname
+    Start-CMContentDistribution -Applicationname $group.name.Substring(2) -DistributionPointGroupName $dpgroupname | Out-Null
     Write-Host -Foregroundcolor Yellow "Create Application Deployment for $($group.name)"
-    New-CMApplicationDeployment -Collectionname $group.name -Name $group.name.Substring(2) -DeployAction Install -DeployPurpose Available -Usernotification DisplayAll -AvailableDateTime (get-date) -TimeBaseOn LocalTime
+    New-CMApplicationDeployment -Collectionname $group.name -Name $group.name.Substring(2) -DeployAction Install -DeployPurpose Available -Usernotification DisplayAll -AvailableDateTime (get-date) -TimeBaseOn LocalTime | Out-Null
     }
 }
-

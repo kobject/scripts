f4742e8ce392eb02a670f4c7b0301d8843ea109f
Kerkhof <joey.kerkhof@enexis.nl>
2020-08-18 11:37:59 +0200

    changes

--- Microsoft Endpoint Manager (Conf Mgr)/Change_EvalSchedule.ps1
+++ Microsoft Endpoint Manager (Conf Mgr)/Change_EvalSchedule.ps1
@@ -1,23 +1,21 @@
 [CmdletBinding()]
 Param(
-    [parameter(Mandatory=$true, HelpMessage="Enter sitecode of ConfigMgr server")] [string]$SMSProvider,
-    [parameter(Mandatory=$false, HelpMessage="")] [string]$Type,
-    [parameter(Mandatory=$true, HelpMessage="")] [string]$LogPath
+    [parameter(Mandatory = $true, HelpMessage = "Enter sitecode of ConfigMgr server")] [string]$SMSProvider,
+    [parameter(Mandatory = $false, HelpMessage = "Taskscheduler or Script (Default = Script)")] [string]$Type,
+    [parameter(Mandatory = $true, HelpMessage = "Enter only the path. Logfile will be created with timestamp")] [string]$LogPath
 
 )
 if (!$Type) { $Type = 'Script' }
 
-Function Get-SiteCode
-{
+Function Get-SiteCode {
     $wqlQuery = “SELECT * FROM SMS_ProviderLocation”
     $a = Get-WmiObject -Query $wqlQuery -Namespace “root\sms” -ComputerName $SMSProvider
     $a | ForEach-Object {
-        if($_.ProviderForLocalSite)
-            {
-                $script:SiteCode = $_.SiteCode
-            }
+        if ($_.ProviderForLocalSite) {
+            $script:SiteCode = $_.SiteCode
+        }
     }
-return $SiteCode
+    return $SiteCode
 }
 
 function Get-TimeStamp {
@@ -27,19 +25,18 @@ function Get-TimeStamp {
 Function Convert-NormalDateToConfigMgrDate {
     [CmdletBinding()]
     param (
-        [parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [string]$starttime
     )
     return [System.Management.ManagementDateTimeconverter]::ToDMTFDateTime($starttime)
 }
 
 Function create-ScheduleToken {
-$SMS_ST_RecurInterval = "SMS_ST_RecurInterval"
-$class_SMS_ST_RecurInterval = [wmiclass]""
-$class_SMS_ST_RecurInterval.psbase.Path ="\\$($SMSProvider)\ROOT\SMS\Site_$($SiteCode):$($SMS_ST_RecurInterval)"
-$script:scheduleToken = $class_SMS_ST_RecurInterval.CreateInstance()
-    if($scheduleToken)
-        {
+    $SMS_ST_RecurInterval = "SMS_ST_RecurInterval"
+    $class_SMS_ST_RecurInterval = [wmiclass]""
+    $class_SMS_ST_RecurInterval.psbase.Path = "\\$($SMSProvider)\ROOT\SMS\Site_$($SiteCode):$($SMS_ST_RecurInterval)"
+    $script:scheduleToken = $class_SMS_ST_RecurInterval.CreateInstance()
+    if ($scheduleToken) {
         $scheduleToken.DayDuration = 0
         $scheduleToken.DaySpan = 0
         $scheduleToken.HourDuration = 0
@@ -48,7 +45,7 @@ $script:scheduleToken = $class_SMS_ST_RecurInterval.CreateInstance()
         $scheduleToken.MinuteDuration = 0
         $scheduleToken.MinuteSpan = 0
         $scheduleToken.StartTime = (Convert-NormalDateToConfigMgrDate $startTime)
-        }
+    }
 }
 
 [datetime]$startTime = [datetime]::Today
@@ -57,42 +54,39 @@ create-ScheduleToken
 
 
 $Collections = Get-WmiObject -Class SMS_Collection -Namespace root\sms\site_$SiteCode
+Write-Verbose "Getting "
 
-If($Type -eq 'Script'){
-    foreach ($Collection in $Collections)
-        {
-            try
-                {
-                    $Coll = Get-WmiObject -Class SMS_Collection -Namespace root\sms\site_$SiteCode -ComputerName $SMSProvider -Filter "CollectionID ='$($Collection.CollectionID)'"
-                    $Coll = [wmi]$Coll.__PATH
-                    $Coll.RefreshSchedule = $scheduletoken
-                    $Coll.RefreshType = 2
-                    $Coll.put() | Out-Null
-                    Write-Verbose "Successfully edited Collection $($Coll.Name)."
-                }
-            catch
-                {
-                    Write-Verbose "$($Coll.Name) could not be edited."
-                }
+If ($Type -eq 'Script') {
+    foreach ($Collection in $Collections) {
+        try {
+            $Coll = Get-WmiObject -Class SMS_Collection -Namespace root\sms\site_$SiteCode -ComputerName $SMSProvider -Filter "CollectionID ='$($Collection.CollectionID)'"
+            $Coll = [wmi]$Coll.__PATH
+            $Coll.RefreshSchedule = $scheduletoken
+            $Coll.RefreshType = 2
+            $Coll.put() | Out-Null
+            Write-Verbose "$(Get-TimeStamp) Successfully edited Collection $($Coll.Name)."
+            Write-Output "$(Get-TimeStamp) Successfully edited Collection $($Coll.Name)." | Out-File $LogPath\$(Get-TimeStamp)-Change_Col_Eval_Script.log -Append
         }
+        catch {
+            Write-Verbose "$(Get-TimeStamp) $($Coll.Name) could not be edited."
+        }
+    }
 }     
- 
-If($Type -eq 'TaskScheduler'){   
-    foreach ($Collection in $Collections)
-    {
-        try
-            {
-                $Coll = Get-WmiObject -Class SMS_Collection -Namespace root\sms\site_P02 -ComputerName $SMSProvider -Filter "CollectionID ='$($Collection.CollectionID)' and RefreshType = '6'"
-                $Coll = [wmi]$Coll.__PATH
-                $Coll.RefreshSchedule = $scheduletoken
-                $Coll.RefreshType = 2
-                $Coll.put() | Out-Null
-                Write-Verbose "Successfully edited Collection $($Coll.Name)."
-                Write-Output "Successfully edited Collection $($Coll.Name)." | Out-File $LogPath -Append
-            }
-        catch
-            {
-                Write-Verbose "$($Coll.Name) could not be edited."
-            }
+
+#Get all newly added Collections
+If ($Type -eq 'TaskScheduler') {   
+    foreach ($Collection in $Collections) {
+        try {
+            $Coll = Get-WmiObject -Class SMS_Collection -Namespace root\sms\site_P02 -ComputerName $SMSProvider -Filter "CollectionID ='$($Collection.CollectionID)' and RefreshType = '6'"
+            $Coll = [wmi]$Coll.__PATH
+            $Coll.RefreshSchedule = $scheduletoken
+            $Coll.RefreshType = 2
+            $Coll.put() | Out-Null
+            Write-Verbose "$(Get-TimeStamp) Successfully edited Collection $($Coll.Name)."
+            Write-Output "$(Get-TimeStamp) Successfully edited Collection $($Coll.Name)." | Out-File $LogPath\$(Get-TimeStamp)-Change_Col_Eval_TaskScheduler.log -Append
+        }
+        catch {
+            Write-Verbose "$(Get-TimeStamp) $($Coll.Name) could not be edited."
+        }
     }
 }

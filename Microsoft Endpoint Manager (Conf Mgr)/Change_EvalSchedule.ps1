a49f1a1cb8288ab9040b95605df3254f31d820c2
Kerkhof <joey.kerkhof@enexis.nl>
2020-09-16 17:15:02 +0200

    changes in quotes

--- Microsoft Endpoint Manager (Conf Mgr)/Change_EvalSchedule.ps1
+++ Microsoft Endpoint Manager (Conf Mgr)/Change_EvalSchedule.ps1
@@ -8,8 +8,8 @@ Param(
 if (!$Type) { $Type = 'Script' }
 
 Function Get-SiteCode {
-    $wqlQuery = “SELECT * FROM SMS_ProviderLocation”
-    $a = Get-WmiObject -Query $wqlQuery -Namespace “root\sms” -ComputerName $SMSProvider
+    $wqlQuery = 'SELECT * FROM SMS_ProviderLocation'
+    $a = Get-WmiObject -Query $wqlQuery -Namespace 'root\sms' -ComputerName $SMSProvider
     $a | ForEach-Object {
         if ($_.ProviderForLocalSite) {
             $script:SiteCode = $_.SiteCode
